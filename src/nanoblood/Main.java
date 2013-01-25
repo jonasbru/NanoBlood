1ecb4a449c2f9422d352aee7e926dc2ae7d14e37
jonas <jonasbru@wanadoo.fr>
2013-01-25 23:17:53 +0100

    config de base slick

--- src/nanoblood/Main.java
+++ src/nanoblood/Main.java
@@ -5,9 +5,11 @@
 
 package nanoblood;
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
+import java.lang.reflect.Field;
 
 /**
  *
@@ -15,20 +17,42 @@ import org.newdawn.slick.state.StateBasedGame;
  */
 public class Main extends StateBasedGame{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    static int height = 650;
+    static int width = 1200;
+    static boolean fullscreen = false;
+    static boolean showFPS = true;
+    static String title = "NanoBlood";
+    static int fpslimit = 60;
+    public static final int MAINMENU = 0;
+    public static final int GAMEPLAY = 1;
+    public static final int GAMEEND = 2;
+    public static final int WAIT = 3;
 
+    public static void main(String[] args) throws SlickException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+
+        //Hack for set the library path
+        System.setProperty( "java.library.path", "./slick/lib" );
+        Field fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
+        fieldSysPath.setAccessible( true );
+        fieldSysPath.set( null, null );
+
+        AppGameContainer app = new AppGameContainer(new Main());
+        app.setDisplayMode(Main.width, Main.height, Main.fullscreen);
+        app.setSmoothDeltas(true);
+        app.setTargetFrameRate(Main.fpslimit);
+        app.setShowFPS(false);
+        app.start();
     }
 
+
+
     public Main() {
-        super("NanoBlood");
+        super(title);
     }
 
     @Override
     public void initStatesList(GameContainer gc) throws SlickException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.addState(new MainMenu(MAINMENU));
     }
 
 }
---
+++ src/nanoblood/MainMenu.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nanoblood;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author jonas
+ */
+public class MainMenu extends BasicGameState{
+
+    int stateID = -1;
+
+    Image test;
+   
+
+    MainMenu(int stateID) {
+        this.stateID = stateID;
+    }
+
+    @Override
+    public int getID() {
+        return stateID;
+    }
+
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        test = new Image("sprites/Narwhals_breach.jpg");
+    }
+
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
+        test.draw(0, 0);
+        
+    }
+
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+    }
+
+}
