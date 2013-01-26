c4ab447020d5efac368d6cda3d4a837b67ea97b7
tdubourg <plop@plop.fake>
2013-01-26 01:56:32 +0100

    WIP on the main menu, adding a sound button

--- src/nanoblood/MainMenu.java
+++ src/nanoblood/MainMenu.java
@@ -9,21 +9,25 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.gui.AbstractComponent;
 import org.newdawn.slick.gui.ComponentListener;
 import org.newdawn.slick.gui.GUIContext;
 import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+
 /**
  *
  * @author jonas
  */
-public class MainMenu extends BasicGameState{
+public class MainMenu extends BasicGameState implements ComponentListener {
 
     int stateID = -1;
 
     Image bg;
+	private CustomMouseOverArea sound;
+	protected static boolean SOUND_ON = true;
    
 
     MainMenu(int stateID) {
@@ -37,34 +41,34 @@ public class MainMenu extends BasicGameState{
 
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
         bg = new Image("sprites/main_menu.jpg");
-		sound = new CustomMouseOverArea(gc, , stateID, stateID, null)
+		sound = new CustomMouseOverArea(gc, Sprite.getImage("sprites/sound_menu_ON.png"), (int)0.8 * Main.width, (int) 0.8 * Main.height, this);
+		sound.setSelectedImage(Sprite.getImage("sprites/sound_menu_ON.png"));
     }
 
     public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
         bg.draw(0, 0);
-        
+        sound.render(gc, grphcs);
     }
 
     public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
     }
 
-}
+	public void componentActivated(AbstractComponent source) {
+		if (sound == source) {
+			SOUND_ON = false;
+		}
+	}
 
+}
 class CustomMouseOverArea extends MouseOverArea {
 
-    private String perso;
     private boolean selected = false;
 	private Image img;
 	private Image selectedImg;
 
     public CustomMouseOverArea(GUIContext container, Image image, int x, int y, ComponentListener listener) {
         super(container, image, x, y, listener);
-
-        perso = image.getResourceReference();
-    }
-
-    public String getPerso() {
-        return perso;
+		this.img = image;
     }
 
     public boolean isSelected() {
