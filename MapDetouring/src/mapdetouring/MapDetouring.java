c56a02438bdb62cc9fd14d70ff2a7eb440c3a723
tdubourg <plop@plop.fake>
2013-01-27 02:40:11 +0100

    Adding a new project that will be used to preprocess the maps and find the environment obstacles

---
+++ MapDetouring/src/mapdetouring/MapDetouring.java
@@ -0,0 +1,19 @@
+
+package mapdetouring;
+
+/**
+ *
+ * @author jammers
+ */
+public class MapDetouring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: mapDetouring PNGImageName");
+            System.exit(0);
+        }
+    }
+}
