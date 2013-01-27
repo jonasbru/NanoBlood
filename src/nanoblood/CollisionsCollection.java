dfa38da02e12cbd6fe8ef11eec037caa2409dd5a
tdubourg <plop@plop.fake>
2013-01-27 07:00:13 +0100

    WIP on CollisionsCollection to inject collisions into the GamePlay world

--- src/nanoblood/CollisionsCollection.java
+++ src/nanoblood/CollisionsCollection.java
@@ -7,6 +7,11 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.collision.shapes.ShapeType;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.World;
 
 /**
  *
@@ -14,6 +19,7 @@ import java.util.Scanner;
  */
 public class CollisionsCollection {
     List<Point2D> ptsList;
+    Body body;
     int radius;
     public static CollisionsCollection fromFile(String path, int radius) throws FileNotFoundException {
         List<Point2D> ptsList = new ArrayList<Point2D>(100);
@@ -28,6 +34,21 @@ public class CollisionsCollection {
     public CollisionsCollection(List<Point2D> ptsList, int radius) {
         this.ptsList = ptsList;
         this.radius = radius;
+    }
+    
+    public void injectIntoWorld(World w, float x, float y) {
+        BodyDef bdef = new BodyDef();
+		bdef.position.set(x, y);
+		body = w.createBody(bdef);
+        
+		for(Point2D p : ptsList) {
+			CircleShape shape = new CircleShape();
+			shape.m_type = ShapeType.CIRCLE;
+			shape.m_radius = this.radius;
+			shape.m_p.x = (float) p.getX();
+			shape.m_p.y = (float) p.getY();
+			body.createFixture(shape, 1.0f);//density=1.0
+		}
     }
     
 }
